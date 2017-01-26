6dfa6170aabc0f95e303f5cf781a1384949651c2
Nataliya Kryvych <nkryvych@gmail.com>
2017-01-26 15:17:24 +0100

    added separate timeserie info/data

--- src/main/java/ch/epfl/cryos/osper/measurement/controller/TimeseriesController.java
+++ src/main/java/ch/epfl/cryos/osper/measurement/controller/TimeseriesController.java
@@ -4,6 +4,8 @@ import ch.epfl.cryos.osper.measurement.ApplicationFields;
 import ch.epfl.cryos.osper.measurement.dto.TimeserieDto;
 import ch.epfl.cryos.osper.measurement.dto.TimeserieQueryDto;
 import ch.epfl.cryos.osper.measurement.model.Measurand;
+import ch.epfl.cryos.osper.measurement.model.Measurement;
+import ch.epfl.cryos.osper.measurement.model.Timeserie;
 import ch.epfl.cryos.osper.measurement.service.MeasurandService;
 import ch.epfl.cryos.osper.measurement.service.TimeserieService;
 import io.swagger.annotations.*;
@@ -53,15 +55,60 @@ public class TimeseriesController {
     public TimeserieDto getTimeserieById(
             @PathVariable(value = "timeserieId") @ApiParam(value = "Timeserie ID", required = true) Long timeserieId,
             @ApiIgnore TimeserieQueryDto query
-//            @RequestParam(value ="from", required = false)
-//            @DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mmZ") Date from,
-//            @RequestParam(value ="to", required = false)
-//            @DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mmZ") Date to
     ) {
 
         System.out.println("!!! QUERY = " + query);
 
         return timeserieService.getTimeserieForQuery(timeserieId, query);
+    }
+
+
+    @ResponseStatus(value = HttpStatus.OK)
+    @RequestMapping(
+            value = "timeseries/{timeserieId}/info",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get data", notes = "Returns timeserie metadata and data JSON format. ", response = String.class)
+
+    public Timeserie getTimeserieInfoById(
+            @PathVariable(value = "timeserieId") @ApiParam(value = "Timeserie ID", required = true) Long timeserieId
+    ) {
+
+        return timeserieService.getTimeserieInfo(timeserieId);
+    }
+
+    @ResponseStatus(value = HttpStatus.OK)
+    @RequestMapping(
+            value = "timeseries/{timeserieId}/data",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get data", notes = "Returns timeserie data JSON format. ", response = String.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "from", value = "Start of the timespan " + ZONED_DATE_TIME, required = false, paramType = "query", defaultValue = "2016-11-17T13:00Z"),
+            @ApiImplicitParam(name = "until", value = "End of the timespan " + ZONED_DATE_TIME, required = false, paramType = "query", defaultValue = "2016-11-17T18:00Z"),
+            @ApiImplicitParam(name = "limit", value = "Row number limit ", required = false, paramType = "query")
+    })
+
+    public List<Measurement> getTimeserieDataById(
+            @PathVariable(value = "timeserieId") @ApiParam(value = "Timeserie ID", required = true) Long timeserieId,
+            @ApiIgnore TimeserieQueryDto query
+    ) {
+
+        System.out.println("!!! QUERY = " + query);
+
+        return timeserieService.getMeasurementsForTimeserie(timeserieId, query);
+    }
+
+    @ResponseStatus(value = HttpStatus.OK)
+    @RequestMapping(
+            value = "timeseries",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get data", notes = "Returns timeserie metadata for a given station Id in JSON format. ", response = String.class)
+
+
+    public List<Timeserie> getAllTimeseries() {
+        return timeserieService.getAllTimeseries();
     }
 
 
@@ -73,12 +120,13 @@ public class TimeseriesController {
     @ApiOperation(value = "Get data", notes = "Returns timeserie metadata for a given station Id in JSON format. ", response = String.class)
 
 
-    public List<TimeserieDto> getTimeseriesForStation(
+    public List<Timeserie> getTimeseriesForStation(
             @PathVariable(value = "stationId") @ApiParam(value = "station id", required = true) Long stationId
     ) {
 
         return timeserieService.getForStation(stationId);
     }
+
 
     @ResponseStatus(value = HttpStatus.OK)
     @RequestMapping(
--- src/main/java/ch/epfl/cryos/osper/measurement/service/TimeserieService.java
+++ src/main/java/ch/epfl/cryos/osper/measurement/service/TimeserieService.java
@@ -2,6 +2,7 @@ package ch.epfl.cryos.osper.measurement.service;
 
 import ch.epfl.cryos.osper.measurement.dto.TimeserieDto;
 import ch.epfl.cryos.osper.measurement.dto.TimeserieQueryDto;
+import ch.epfl.cryos.osper.measurement.model.Measurement;
 import ch.epfl.cryos.osper.measurement.model.Timeserie;
 import ch.epfl.cryos.osper.measurement.repository.MeasurementRepository;
 import ch.epfl.cryos.osper.measurement.repository.TimeserieRepository;
@@ -9,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Created by kryvych on 23/12/16.
@@ -55,13 +55,41 @@ public class TimeserieService {
 
     }
 
-    public List<TimeserieDto> getForStation(Long stationId) {
+    public List<Measurement> getMeasurementsForTimeserie(Long id, TimeserieQueryDto query) {
 
-        return timeserieRepository.findByStationId(stationId)
-                .stream()
-                .map(x -> new TimeserieDto(x, null))
-                .collect(Collectors.toList());
+        if (query.getFrom() != null && query.getUntil() != null) {
+            return measurementRepository.findByTimeserieIdAndMeasureDateBetween(id, query.getFrom(), query.getUntil());
+        }
+        if(query.getFrom() != null) {
+            return measurementRepository.findByTimeserieIdAndMeasureDateGreaterThanEqual(id, query.getFrom());
+        }
+        if(query.getUntil() != null) {
+           return measurementRepository.findByTimeserieIdAndMeasureDateLessThanEqual(id, query.getUntil());
+        }
 
+        return measurementRepository.findByTimeserieId(id);
+
+    }
+
+    public Timeserie getTimeserieInfo(Long timeserieId) {
+        return timeserieRepository.findOne(timeserieId);
+    }
+
+//    public List<TimeserieDto> getForStation(Long stationId) {
+//
+//        return timeserieRepository.findByStationId(stationId)
+//                .stream()
+//                .map(x -> new TimeserieDto(x, null))
+//                .collect(Collectors.toList());
+//
+//    }
+
+    public List<Timeserie> getForStation(Long stationId) {
+        return timeserieRepository.findByStationId(stationId);
+    }
+
+    public List<Timeserie> getAllTimeseries() {
+        return timeserieRepository.findAll();
     }
 
 }
